53180107d71c39c58f6f2bc782da56a6a37511bb
Techarerm <126402510+Techarerm@users.noreply.github.com>
2023-04-16 19:08:30 +0800

    Add files via upload

---
+++ cert/test.ps1
@@ -0,0 +1,3 @@
+﻿$cert = New-SelfSignedCertificate -DnsName test.app.com -CertStoreLocation cert:\LocalMachine\My -type CodeSigning
+$pwd = ConvertTo-SecureString -String "Veyon-Killer" -Force -AsPlainText
+Export-PfxCertificate -cert $cert -FilePath testcert.pfx -Password $pwd
